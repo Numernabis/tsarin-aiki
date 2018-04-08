bbb2e6e98e67f23becd8a2ea0d47fe798f274124
Numernabis <luigi.zielone@gmail.com>
2018-04-08 19:20:50 +0200

    set 4 ex 1a - completed

--- cw04/zad1a/main.c
+++ cw04/zad1a/main.c
@@ -2,11 +2,12 @@
   author: Ludi
   file:   main.c
   start:  05.04.2018
-  end:    []
-  lines:  65
+  end:    08.04.2018
+  lines:  62
 */
 #define _XOPEN_SOURCE 500
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -15,29 +16,20 @@
 #define SIGINT  2
 #define SIGSTP 20
 int STOPPED = 0;
+char stamp[20];
 
-void handler_int(int signum);
-void handler_start(int signum);
-void handler_stop(int signum);
-
-int time_stamp() {
-    time_t rawtime;
-    time(&rawtime);
-    struct tm *stamp = localtime(&rawtime);
-
-    printf("\n%ld", rawtime);
-    printf("\n%s", asctime(stamp));
-    return 0;
-}
+void handler_sigint(int signum);
+void handler_sigstp(int signum);
+void time_stamp(void);
 
 int main(int argc, char** argv) {
-    struct sigaction act;
-    act.sa_handler = handler_stop;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
+    struct sigaction stp;
+    stp.sa_handler = handler_sigstp;
+    sigemptyset(&stp.sa_mask);
+    stp.sa_flags = 0;
 
-    sigaction(SIGSTP, &act, NULL);
-    signal(SIGINT, handler_int);
+    sigaction(SIGSTP, &stp, NULL);
+    signal(SIGINT, handler_sigint);
 
     /* ============================== */
     while(1){
@@ -49,17 +41,22 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-void handler_stop(int signum) {
-    STOPPED = 1;
-    printf("\nOczekuje na CTRL+Z - kontynuacja albo CTR+C - zakonczenie programu. \n");
-    signal(SIGSTP, handler_start);
+void time_stamp() {
+    time_t rawtime;
+    time(&rawtime);
+    strftime(stamp, 20, "%Y-%m-%d %H:%M:%S", localtime(&rawtime));
+    printf("%s\n", stamp);
 }
-void handler_start(int signum) {
-    STOPPED = 0;
-    signal(SIGSTP, handler_stop);
+void handler_sigstp(int signum) {
+    if (!STOPPED) {
+        STOPPED = 1;
+        printf("\nOczekuję na CTRL+Z - kontynuacja albo CTR+C - zakonczenie programu\n");
+    } else {
+        STOPPED = 0;
+        return;
+    }
 }
-void handler_int(int signum) {
-    printf("\nOdebrano sygnal SIGINT.\n");
-    raise(2);
-    signal(SIGINT, SIG_DFL);
+void handler_sigint(int signum) {
+    printf("\nOdebrano sygnał SIGINT.\n");
+    exit(0);
 }
