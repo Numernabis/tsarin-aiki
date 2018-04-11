c5eac13f0a72070970105771ef9863f7bd8e471d
Numernabis <luigi.zielone@gmail.com>
2018-04-11 21:34:52 +0200

    set 4 ex 3 - something works

--- cw04/zad3/main.c
+++ cw04/zad3/main.c
@@ -16,26 +16,103 @@
 #include <time.h>
 
 int L = 0;
-int Type = 0;
-int sig_cnt = 0;
+int TYPE = 0;
+int CHILD_CNT = 0;
+int PARENT_CNT = 0;
+int SEND = 0;
+
+void set_handlers_parent();
+void set_handlers_child();
+void send_signal(pid_t);
+void handler_sig1_parent(int, siginfo_t*, void*);
+void handler_sig1_child(int, siginfo_t*, void*);
+void handler_sig2_child(int, siginfo_t*, void*);
+
+#define CREATE_SIGACT                                                          \
+    struct sigaction act;                                                      \
+    memset(&act, 0, sizeof(act));                                              \
+    sigset_t real_mask;                                                        \
+    sigfillset(&real_mask);                                                    \
+
+#define SIG_HANDLER(signal, func) {                                            \
+    act.sa_sigaction = &(func);                                                \
+    sigaction((signal), &act, NULL);                                           \
+}
 
 int main(int argc, char** argv) {
     if (argc != 3) {
         printf("Wrong number of arguments. Proper call:\n"
-               "./program  L  Type\n"
-               "where: L - number of requests, Type - sending method \n");
+               "./program  L  TYPE\n"
+               "where: L - number of requests, TYPE - sending method \n");
         return 2;
     }
-    if ((L = atoi(argv[1])) == 0 || (Type = atoi(argv[2])) == 0 || L < 0 || Type < 0) {
-        printf("Parameters L, Type must be positive integers.\n");
+    if ((L = atoi(argv[1])) == 0 || (TYPE = atoi(argv[2])) == 0 || L < 0 || TYPE < 0) {
+        printf("Parameters L, TYPE must be positive integers.\n");
         return 2;
     }
-    if (Type < 1 || Type > 3) {
-        printf("Parameter Type must be 1, 2 or 3.\n");
+    if (TYPE < 1 || TYPE > 3) {
+        printf("Parameter TYPE must be 1, 2 or 3.\n");
         return 2;
     }
     /* ============================== */
-
+    pid_t pid = fork();
+    if (pid != 0) {
+        set_handlers_parent();
+        sleep(2);
+        for(int i = 0; i < L; i++) {
+            send_signal(pid);
+        }
+        if (TYPE != 3) kill(pid, SIGUSR2);
+        else kill(pid, SIGRTMIN + 1);
+    } else {
+        set_handlers_child();
+        while(1) { pause(); }
+    }
+    printf("Send %d signals from parent\n", SEND);
+    printf("Received %d signals at parent\n", PARENT_CNT);
     /* ============================== */
     return 0;
+}
+
+void set_handlers_parent() {
+    CREATE_SIGACT
+    SIG_HANDLER(SIGRTMIN, handler_sig1_parent);
+    SIG_HANDLER(SIGUSR1, handler_sig1_parent);
+}
+void set_handlers_child() {
+    CREATE_SIGACT
+    act.sa_mask = real_mask;
+    act.sa_flags = SA_RESTART;
+    SIG_HANDLER(SIGRTMIN, handler_sig1_child);
+    SIG_HANDLER(SIGRTMIN + 1, handler_sig2_child);
+    SIG_HANDLER(SIGUSR1, handler_sig1_child);
+    SIG_HANDLER(SIGUSR2, handler_sig2_child);
+}
+
+void send_signal(pid_t pid) {
+    int sig = SIGUSR1;
+    if (TYPE == 3) sig = SIGRTMIN;
+
+    if (kill(pid, sig) == -1) {
+        printf("Error: sending signal failed.\n");
+        exit(1);
+    } else {
+        SEND++;
+    }
+    if (TYPE == 2) sleep(1);
+}
+
+void handler_sig1_parent(int num, siginfo_t *info, void *context) {
+    PARENT_CNT++;
+}
+void handler_sig1_child(int num, siginfo_t *info, void *context) {
+    int sig = SIGUSR1;
+    if (TYPE == 3) sig = SIGRTMIN;
+    CHILD_CNT++;
+    kill(getppid(), sig);
+}
+void handler_sig2_child(int num, siginfo_t *info, void *context) {
+    printf("Process ended\n");
+    printf("Received %d signals at child\n", CHILD_CNT);
+    exit(0);
 }
